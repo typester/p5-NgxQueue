71640059b29856785bb2e0213818ad26f66b37c9
Daisuke Murase <typester@cpan.org>
2013-03-12 17:52:02 +0900

    initial commit

---
+++ cpanfile
@@ -0,0 +1,7 @@
+requires "perl", "5.008001";
+
+on "author" => sub {
+    requires "Module::Install::XSUtil";
+    requires "Module::Install::CPANfile";
+};
+
---
+++ lib/NgxQueue.pm
@@ -0,0 +1,20 @@
+package NgxQueue;
+use strict;
+use warnings;
+use XSLoader;
+
+our $VERSION = '0.01';
+
+XSLoader::load __PACKAGE__, $VERSION;
+
+1;
+
+=head1 NAME
+
+NgxQueue - 
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+=cut
